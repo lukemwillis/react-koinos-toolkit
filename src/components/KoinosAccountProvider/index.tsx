9a38a19dc645f06797f7ee0fa455fa6fd1220b2a
luke <luke@prescryptive.com>
2023-05-18 16:11:38 -0700

    fix koilib_types

--- src/components/KoinosAccountProvider/index.tsx
+++ src/components/KoinosAccountProvider/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { AccountProvider } from "./AccountProvider";
 import { NameServiceProvider } from "./NameServiceProvider";
 import { ProfileProvider } from "./ProfileProvider";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export const KoinosAccountProvider = ({ children }) => {
   return (
-    <AccountProvider>
-      <NameServiceProvider>
-        <ProfileProvider>{children}</ProfileProvider>
-      </NameServiceProvider>
-    </AccountProvider>
+    <ChakraProvider>
+      <AccountProvider>
+        <NameServiceProvider>
+          <ProfileProvider>{children}</ProfileProvider>
+        </NameServiceProvider>
+      </AccountProvider>
+    </ChakraProvider>
   );
 };
